a29813cadb47d7d66db1c88e731aa884d3a1a448
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2020-07-11 20:28:23 -0300

    [ADD] HiroAnimation

--- Assets/Scripts/Player/GunBehav.cs
+++ Assets/Scripts/Player/GunBehav.cs
@@ -7,15 +7,31 @@ public class GunBehav : MonoBehaviour
     Transform gunPoint;
     [SerializeField] GameObject bullet; 
     [SerializeField] float bulletSpeed = 10;
+    [SerializeField] float gunCD=2f;
+    float savegunCD;
+    public bool shooting = false;
     void Start()
     {
+        savegunCD=gunCD;
         gunPoint = GameObject.Find(this.name+"/gunPoint").transform;
     }
 
+    void Update()
+    {
+        gunCD -= Time.deltaTime;
+        if(gunCD<=0)
+        {
+            shooting = true;
+        } else shooting = false;
+    }
     public void Fire()
     {
-        GameObject shot = Instantiate(bullet, gunPoint.position, Quaternion.identity);
-        shot.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(Vector3.right * bulletSpeed);
+        if(gunCD<=0)
+        {
+            GameObject shot = Instantiate(bullet, gunPoint.position, Quaternion.identity);
+            shot.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(Vector3.right * bulletSpeed);
+            gunCD = savegunCD;
+        } 
     }
 
 }
