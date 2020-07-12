dda724d6df2ff5a89e4c7ac5e149e5dfe73dca01
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2020-07-12 12:26:22 -0300

    [ADD] SecondPhase Start

---
+++ Assets/Scripts/SecondPhaseStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondPhaseStart : MonoBehaviour
+{
+    [SerializeField] Vector3 secondStart;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = secondStart;
+        }
+    }
+}
